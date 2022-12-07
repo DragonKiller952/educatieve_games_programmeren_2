0d73cb9c288cb2206e33a195d68ddf7c3c4926b0
DragonKiller952 <qjdegroot@gmail.com>
2022-12-07 13:26:01 +0100

    Add unity Project

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject Settings;
+    public GameObject Loading;
+    public Slider loadingBar;
+
+    /// <summary>
+    /// Starts the scene containing the game with a loading screen
+    /// </summary>
+    public void StartGame()
+    {
+        StartCoroutine(StartGameAsynchronously("Main_scene"));
+    }
+
+    /// <summary>
+    /// Opens the given Tutorial screen
+    /// </summary>
+    public void OpenSettings()
+    {
+        Settings.SetActive(true);
+    }
+
+
+    /// <summary>
+    /// Closes the given Tutorial screen
+    /// </summary>
+    public void CloseSettings()
+    {
+        Settings.SetActive(false);
+    }
+
+    /// <summary>
+    /// Closes the application
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator StartGameAsynchronously(string scene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+        Loading.SetActive(true);
+
+        //Updates the loadingbar during the loading
+        while (!operation.isDone)
+        {
+            loadingBar.value = operation.progress;
+            yield return null;
+        }
+    }
+}
